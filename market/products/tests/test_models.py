6f6d2bf44f7dcb4a5d96b331a4143b3d582403ac
Роман Матвеенко <romses1994@mail.ru>
2023-04-07 16:58:29 +0200

    Добавлены тесты для моделей приложений

---
+++ market/products/tests/test_models.py
@@ -0,0 +1,99 @@
+from django.test import TestCase
+from products.models import Product, Property, ProductProperty
+
+
+class ProductModelTest(TestCase):
+    """Класс тестов модели Продукт"""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.property = Property.objects.create(name='тестовая характеристика')
+        cls.product = Product.objects.create(
+            name='Тестовый продукт',
+        )
+        cls.product.property.set([cls.property])
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        ProductModelTest.property.delete()
+        ProductModelTest.product.delete()
+
+    def test_verbose_name(self):
+        product = ProductModelTest.product
+        field_verboses = {
+            'name': 'наименование',
+            'property': 'характеристики',
+        }
+        for field, expected_value in field_verboses.items():
+            with self.subTest(field=field):
+                self.assertEqual(product._meta.get_field(field).verbose_name, expected_value)
+
+    def test_name_max_length(self):
+        product = ProductModelTest.product
+        max_length = product._meta.get_field('name').max_length
+        self.assertEqual(max_length, 512)
+
+
+class PropertyModelTest(TestCase):
+    """Класс тестов модели Свойство продукта"""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.property = Property.objects.create(name='тестовая характеристика')
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        PropertyModelTest.property.delete()
+
+    def test_verbose_name(self):
+        property = ProductModelTest.property
+        field_verboses = {
+            'name': 'наименование',
+        }
+        for field, expected_value in field_verboses.items():
+            with self.subTest(field=field):
+                self.assertEqual(property._meta.get_field(field).verbose_name, expected_value)
+
+    def test_name_max_length(self):
+        property = ProductModelTest.property
+        max_length = property._meta.get_field('name').max_length
+        self.assertEqual(max_length, 512)
+
+
+class ProductPropertyModelTest(TestCase):
+    """Класс тестов модели Значение свойства продукта"""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.property = Property.objects.create(name='тестовая характеристика')
+        cls.product = Product.objects.create(
+            name='Тестовый продукт',
+        )
+        cls.product_property = ProductProperty.objects.create(product=cls.product, property=cls.property,
+                                                              value='тестовое значение характеристики')
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        ProductPropertyModelTest.product.delete()
+        ProductPropertyModelTest.property.delete()
+        ProductPropertyModelTest.product_property.delete()
+
+    def test_verbose_name(self):
+        product_property = ProductPropertyModelTest.product_property
+        field_verboses = {
+            'value': 'значение',
+        }
+        for field, expected_value in field_verboses.items():
+            with self.subTest(field=field):
+                self.assertEqual(product_property._meta.get_field(field).verbose_name, expected_value)
+
+    def test_value_max_length(self):
+        product_property = ProductPropertyModelTest.product_property
+        max_length = product_property._meta.get_field('value').max_length
+        self.assertEqual(max_length, 128)
