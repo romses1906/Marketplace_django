73c9e921be697bcb97fb5a9c0402ec745194fe10
Роман Матвеенко <romses1994@mail.ru>
2023-04-09 21:54:40 +0200

    Добавлена модель категории каталога

--- market/products/admin.py
+++ market/products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from products.models import Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Регистрация модели Категория в админке"""
+    list_display = ['name', 'is_active', 'product']
+    list_filter = ['is_active', ]
+
+
+admin.site.register(Category, CategoryAdmin)
--- market/products/models.py
+++ market/products/models.py
@@ -18,3 +18,20 @@ class ProductProperty(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     property = models.ForeignKey(Property, on_delete=models.PROTECT)
     value = models.CharField(max_length=128, verbose_name=_("значение"))
+
+
+class Category(models.Model):
+    """Категория товара"""
+    name = models.CharField(max_length=512, verbose_name=_("наименование"))
+    description = models.CharField(max_length=512, verbose_name=_("описание"))
+    image = models.ImageField(upload_to='departments/', verbose_name=_("иконка"))
+    product = models.ForeignKey(Product, related_name='category', on_delete=models.PROTECT)
+    is_active = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = _('категория')
+        verbose_name_plural = _('категории')
+        ordering = ['is_active']
+
+    def __str__(self):
+        return self.name
