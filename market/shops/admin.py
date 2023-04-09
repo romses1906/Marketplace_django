5a1be0796f50b78a77c23a6e6f46b18bc1e87634
Nikolay Osipov <nick_osipov@mail.ru>
2023-04-09 14:23:28 +0300

    Created banner models in models.py shops apps. And integrated into the admin panel.

--- market/shops/admin.py
+++ market/shops/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Banner
 
-# Register your models here.
+
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_active',)
+    list_filter = ('is_active',)
+    search_fields = ('title', 'description',)
+
+    def get_queryset(self, request):
+        return Banner.objects.get_active_banners()
+
+
+admin.site.register(Banner, BannerAdmin)
--- market/shops/models.py
+++ market/shops/models.py
@@ -14,3 +14,22 @@ class Offer(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.PROTECT)
     product = models.ForeignKey("products.Product", on_delete=models.PROTECT)
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("цена"))
+
+
+class BannerManager(models.Manager):
+    """Менеджер,для отображения активных баннеров на главной странице."""
+    def get_active_banners(self):
+        return self.filter(is_active=True).order_by('?')[:3]
+
+
+class Banner(models.Model):
+    """Баннеры магазина"""
+    image = models.ImageField(upload_to='banners/')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    is_active = models.BooleanField(default=False)
+    link = models.URLField()
+    objects = BannerManager()
+
+    def __str__(self):
+        return self.title
