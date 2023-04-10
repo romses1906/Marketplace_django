ebc41676bad9ca322ebfceaa4c3af34e807218d7
Nikolay Osipov <nick_osipov@mail.ru>
2023-04-10 08:58:16 +0300

    Created banner models in models.py shops apps. And integrated into the admin panel.

--- market/shops/admin.py
+++ market/shops/admin.py
@@ -3,11 +3,14 @@ from .models import Banner
 
 
 class BannerAdmin(admin.ModelAdmin):
+    """Используется для настройки отображения
+     и поведения модели баннера в Django Admin."""
     list_display = ('title', 'is_active',)
     list_filter = ('is_active',)
     search_fields = ('title', 'description',)
 
     def get_queryset(self, request):
+        """Фильтрует список и включает только активные баннеры."""
         return Banner.objects.get_active_banners()
 
 
--- market/shops/tests/test_models.py
+++ market/shops/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from shops.models import Shop, Offer
+from shops.models import Shop, Offer, BannerManager, Banner
 from products.models import Product, Property
 
 
@@ -78,3 +78,63 @@ class OfferModelTest(TestCase):
         offer = OfferModelTest.offer
         decimal_places = offer._meta.get_field('price').decimal_places
         self.assertEqual(decimal_places, 2)
+
+
+class BannerManagerTestCase(TestCase):
+    """Класс тестов менеджера баннеров"""
+    def setUp(self):
+        banner1 = Banner.objects.create(
+            title='Banner 1',
+            description='Banner 1 description',
+            is_active=True,
+            link='https://example.com'
+        )
+        banner2 = Banner.objects.create(
+            title='Banner 2',
+            description='Banner 2 description',
+            is_active=True,
+            link='https://example.com'
+        )
+        banner3 = Banner.objects.create(
+            title='Banner 3',
+            description='Banner 3 description',
+            is_active=False,
+            link='https://example.com'
+        )
+        banner4 = Banner.objects.create(
+            title='Banner 4',
+            description='Banner 4 description',
+            is_active=True,
+            link='https://example.com'
+        )
+
+    def test_get_active_banners(self):
+        active_banners = Banner.objects.get_active_banners()
+        self.assertEqual(len(active_banners), 3)
+        for banner in active_banners:
+            self.assertTrue(banner.is_active)
+
+    def test_active_banners_random_order(self):
+        active_banners_1 = Banner.objects.get_active_banners()
+        active_banners_2 = Banner.objects.get_active_banners()
+        self.assertNotEqual(active_banners_1, active_banners_2)
+
+
+class BannerTestCase(TestCase):
+    """Класс тестов модели Баннер"""
+    def test_create_banner(self):
+        banner = Banner.objects.create(
+            image='banners/banner.jpg',
+            title='MAVIC PRO 5 MINI DRONE',
+            description='Get the best phone you ever seen with modern Windows OS plus 70% Off this summer',
+            is_active=True,
+            link='https://example.com'
+        )
+
+        self.assertEqual(banner.image.path, 'banners/banner.jpg')
+        self.assertEqual(banner.title, 'MAVIC PRO 5 MINI DRONE')
+        self.assertEqual(banner.description, 'Get the best phone you ever seen with modern Windows OS plus 70% Off '
+                                             'this summer')
+        self.assertTrue(banner.is_active)
+        self.assertEqual(banner.link, 'https://example.com')
+        self.assertEqual(str(banner), 'MAVIC PRO 5 MINI DRONE')
