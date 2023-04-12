5ce2011cf3206c29a1af0d7ae06ccb7ad1ea9431
Роман Матвеенко <romses1994@mail.ru>
2023-04-12 19:35:08 +0200

    Добавлены тесты модели Категория, исправлены фикстуры модели Категория

--- market/products/tests/test_models.py
+++ market/products/tests/test_models.py
@@ -102,3 +102,66 @@ class ProductPropertyModelTest(TestCase):
         product_property = ProductPropertyModelTest.product_property
         max_length = product_property._meta.get_field('value').max_length
         self.assertEqual(max_length, 128)
+
+
+class CategoryModelTest(TestCase):
+    """Класс тестов модели Категория"""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.category = Category.objects.create(name='тестовая категория', description='тестовое описание категории')
+        cls.category_child = Category.objects.create(name='вложенная категория',
+                                                     description='тестовое описание вложенной категории',
+                                                     parent=cls.category)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+    def test_verbose_name(self):
+        category = CategoryModelTest.category
+        field_verboses = {
+            'name': 'наименование',
+            'description': 'описание',
+            'parent': 'родительская категория'
+        }
+        for field, expected_value in field_verboses.items():
+            with self.subTest(field=field):
+                self.assertEqual(category._meta.get_field(field).verbose_name, expected_value)
+
+    def test_name_max_length(self):
+        category = CategoryModelTest.category
+        max_length = category._meta.get_field('name').max_length
+        self.assertEqual(max_length, 512)
+
+    def test_description_max_length(self):
+        category = CategoryModelTest.category
+        max_length = category._meta.get_field('description').max_length
+        self.assertEqual(max_length, 512)
+
+    def test_blank_fields(self):
+        category = CategoryModelTest.category
+        blank_fields = [
+            'image',
+            'parent',
+        ]
+        for field in blank_fields:
+            with self.subTest(field=field):
+                self.assertTrue(category._meta.get_field(field_name=field).blank)
+
+    def test_null_fields(self):
+        category = CategoryModelTest.category
+        null_fields = [
+            'image',
+            'parent',
+        ]
+        for field in null_fields:
+            with self.subTest(field=field):
+                self.assertTrue(category._meta.get_field(field_name=field).blank)
+
+    def test_parent_field(self):
+        category = CategoryModelTest.category
+        category_child = CategoryModelTest.category_child
+        name_category_parent = category_child.parent.name
+        self.assertEqual(name_category_parent, category.name)
