ca3364d50f3d0cd51d1e7deb7537f04a70738275
Роман Матвеенко <romses1994@mail.ru>
2023-04-18 17:39:11 +0200

    Added view and page for products in category

--- market/products/tests/test_views.py
+++ market/products/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 from products.models import Category
+from shops.models import Offer
 
 
 class CategoriesListViewTest(TestCase):
@@ -24,3 +25,30 @@ class CategoriesListViewTest(TestCase):
 
     def test_categories_count_is_correct(self):
         self.assertTrue(len(self.response.context['categories']) == self.categories.count())
+
+
+class ProductsByCategoryViewTest(TestCase):
+    """ Тестирование представления для отображения товаров конкретной категории """
+    fixtures = [
+        "users.json",
+        "categories.json",
+        "products.json",
+        "shops.json",
+        "offers.json",
+    ]
+
+    def setUp(self):
+        self.client = Client()
+        self.category = Category.objects.get(id=15)
+        self.offers = Offer.objects.select_related('shop', 'product').filter(product__category=self.category)
+        url = reverse("products:products_by_category", kwargs={'pk': self.category.pk})
+        self.response = self.client.get(url)
+
+    def test_view_returns_correct_HTTP_status(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_view_renders_desired_template(self):
+        self.assertTemplateUsed(self.response, "products/products.html")
+
+    def test_products_by_category_count_is_correct(self):
+        self.assertTrue(len(self.response.context['offers']) == self.offers.count())
