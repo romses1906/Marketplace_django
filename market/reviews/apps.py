435968590e346983d76132bb762344ef71852e83
Александр Корсунов <kors21vek@mail.ru>
2023-04-19 09:21:29 +0300

    del doc-string

--- market/reviews/apps.py
+++ market/reviews/apps.py
@@ -2,7 +2,5 @@ from django.apps import AppConfig
 
 
 class ReviewsConfig(AppConfig):
-    """Конфигурационные данные приложения."""
-
     default_auto_field = "django.db.models.BigAutoField"
     name = "reviews"
