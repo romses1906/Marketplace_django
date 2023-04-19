e9b18e03619a1f30be6f7ad4147e1632a6cd75c7
Александр Корсунов <kors21vek@mail.ru>
2023-04-19 09:17:28 +0300

    add app reviews (model, fixture, test)

--- market/reviews/apps.py
+++ market/reviews/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class ReviewsConfig(AppConfig):
+    """Конфигурационные данные приложения."""
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "reviews"
