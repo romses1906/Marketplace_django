0fb9fb86750454381209fc8ec39d3b0ddd0e8e47
Александр Корсунов <kors21vek@mail.ru>
2023-04-20 17:17:03 +0300

    updated display

--- market/users/admin.py
+++ market/users/admin.py
@@ -1,14 +1,41 @@
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from .models import User
-from .forms import CustomUserCreationForm
 
 
 class UserRegAdmin(UserAdmin):
     """Регистрация модели User в админке"""
-    add_form = CustomUserCreationForm
-    model = User
-    list_display = 'email', 'is_superuser', 'is_active'
+
+    list_display = 'email', 'is_superuser', 'is_staff', 'is_active'
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Персональная информация'),
+         {'fields': ('username', 'first_name', 'last_name', 'surname', 'phone_number', 'photo')}),
+        (
+            _('Разрешения'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        (_('Важные даты'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('email', 'password1', 'password2'),
+            },
+        ),
+    )
+    ordering = '-is_superuser', '-is_staff'
 
 
 admin.site.register(User, UserRegAdmin)
