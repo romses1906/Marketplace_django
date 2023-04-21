c1fcd79bf0ec921855ec25302b21ad09a2842653
Александр Корсунов <kors21vek@mail.ru>
2023-04-21 16:08:35 +0300

    add `get_success_url`

--- market/users/views.py
+++ market/users/views.py
@@ -1,12 +1,12 @@
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetConfirmView
-from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.models import Group
 from django.utils.translation import gettext_lazy as _
 from django.db import transaction
+from django.contrib.messages.views import SuccessMessageMixin
 
 from .models import User
 from .forms import CustomUserCreationForm
@@ -17,32 +17,41 @@ class LoginUserView(LoginView):
     template_name = 'users/login.html'
 
 
-class RegisterView(CreateView):
+class RegisterView(SuccessMessageMixin, CreateView):
     """
     Регистрация пользователя. При методе POST, функция сохраняет полученные данные в кастомной модели User.
     """
     template_name = 'users/register.html'
     form_class = CustomUserCreationForm
     queryset = User.objects.all()
+    success_url = reverse_lazy('users:register_user')
+
+    def get_success_url(self, save=False):
+        """
+        Метод класса, определяющий в зависимости от выполнения запроса отправляемое сообщение.
+        """
+        if save:
+            messages.add_message(self.request, messages.INFO, _('Вы успешно зарегистрированы!'))
+        else:
+            messages.add_message(self.request, messages.INFO, _('К сожалению запрос не удался, попробуйте позже!'))
+
+        return self.success_url
 
     def form_valid(self, form):
         """
-        После регистрации, пользователь аутентифицируется и переадресовывается на главную страницу.
-        А также добавляется группа с разрешениями "покупатель".
+        После регистрации, пользователю добавляется группа с разрешениями "покупатель".
         """
         email = form.cleaned_data.get('email')
-        password = form.cleaned_data.get('password1')
+
         try:
             with transaction.atomic():
                 form.save()
-                user = authenticate(email=email, password=password)
-                login(self.request, user)
                 group = Group.objects.get(name='buyer')
                 user = User.objects.get(email=email)
                 user.groups.add(group)
-                return redirect('/')
+                return HttpResponseRedirect(self.get_success_url(save=True))
         except Exception:
-            return HttpResponse(_('К сожалению запрос не удался, попробуйте позже!'))
+            return HttpResponseRedirect(self.get_success_url())
 
 
 class PasswordResetRequestView(PasswordResetView):
