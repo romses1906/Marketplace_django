9c4f3494e8abd6076b4e33efa293fc0229dc8c68
Александр Корсунов <kors21vek@mail.ru>
2023-04-30 22:31:39 +0300

    add service reviews

--- market/reviews/models.py
+++ market/reviews/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from shops.models import Offer
 from users.models import User
+from products.models import Product
 
 
 class Reviews(models.Model):
     """Модель отзывов к продуктам."""
     product = models.ForeignKey(
-        Offer, on_delete=models.CASCADE, null=False, blank=False, related_name='reviews', verbose_name=_('продукт')
+        Product, on_delete=models.CASCADE, related_name='product_reviews',
+        null=False, blank=False, verbose_name=_('продукт')
     )
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name=_('автор отзыва'))
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('время добавления'))
-    update_at = models.DateTimeField(auto_now=True, verbose_name=_('время обновления'))
     content = models.TextField(null=False, blank=False, verbose_name=_('содержимое отзыва'))
 
     class Meta:
@@ -21,4 +21,4 @@ class Reviews(models.Model):
 
     def __str__(self):
         """Переопределение строкового представления модели."""
-        return f'{self.product.product.name}, автор - {self.author.username}'
+        return f'{self.product.name}, автор - {self.author.username}'
