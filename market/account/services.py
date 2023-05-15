111a6ed834d900cd1fa3bc89a2f9e10df73e9dc5
Александр Корсунов <kors21vek@mail.ru>
2023-05-15 15:37:26 +0300

    add register and update shop

--- market/account/services.py
+++ market/account/services.py
@@ -1,12 +1,16 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import Group
 from django.core.exceptions import ValidationError
 from django.core.files.storage import FileSystemStorage
 from django.core.validators import validate_email
 from django.db.models import QuerySet
 from django.http import HttpRequest
 from django.utils.translation import gettext_lazy as _
+
+from shops.models import Shop
+from users.models import User
 
 
 def change_profile(request: HttpRequest, user: QuerySet):
@@ -65,3 +69,62 @@ def change_profile(request: HttpRequest, user: QuerySet):
             login(request, user_login)
         else:
             messages.add_message(request, messages.INFO, _('Пароли не совпадают!'))
+
+
+def create_shop(request: HttpRequest):
+    """Добавление магазина в БД."""
+    name = request.POST.get('name')
+    description = request.POST.get('description')
+    phone_number = request.POST.get('phone')
+    address = request.POST.get('address')
+    email = request.POST.get('mail')
+    user = User.objects.get(pk=request.user.pk)
+
+    if Shop.objects.filter(user_id=user.pk).exists():
+        return _('Регистрация возможна только одного магазина!')
+
+    Shop.objects.create(
+        name=name,
+        description=description,
+        phone_number=phone_number,
+        address=address,
+        email=email,
+        user=user
+    )
+    group = Group.objects.get(name='seller')
+    user.groups.add(group)
+    return _('Магазин успешно добавлен!')
+
+
+def update_shop(request: HttpRequest):
+    """Редактирование магазина."""
+    # изменение наименования магазина
+    if request.POST.get('name'):
+        Shop.objects.filter(user_id=request.user.pk).update(
+            name=request.POST.get('name')
+        )
+    # изменение описания магазина
+    if request.POST.get('description'):
+        Shop.objects.filter(user_id=request.user.pk).update(
+            description=request.POST.get('description')
+        )
+    # изменение номера телефона магазина
+    if request.POST.get('phone'):
+        Shop.objects.filter(user_id=request.user.pk).update(
+            phone_number=request.POST.get('phone')
+        )
+    # изменение адреса магазина
+    if request.POST.get('address'):
+        Shop.objects.filter(user_id=request.user.pk).update(
+            address=request.POST.get('address')
+        )
+    # изменение электронной почты магазина
+    if request.POST.get('mail'):
+        email = request.POST.get('mail')
+        try:
+            validate_email(email)
+            Shop.objects.filter(user_id=request.user.pk).update(
+                email=request.POST.get('mail')
+            )
+        except ValidationError:
+            messages.add_message(request, messages.INFO, _('Email не соответствует требованиям!'))
--- market/account/views.py
+++ market/account/views.py
@@ -1,12 +1,14 @@
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import DetailView, UpdateView
+from django.views.generic import DetailView, UpdateView, CreateView
 
+from shops.models import Shop
 from users.models import User
-from .services import change_profile
+from .services import change_profile, create_shop, update_shop
 
 
 class AccountUser(DetailView):
@@ -15,6 +17,15 @@ class AccountUser(DetailView):
     template_name = 'account/account.j2'
     context_object_name = 'user'
     model = User
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = User.objects.get(pk=self.request.user.pk)
+        if hasattr(user, 'shop'):
+            context['shop'] = True
+            return context
+        context['shop'] = False
+        return context
 
 
 class ProfileUser(SuccessMessageMixin, UpdateView):
@@ -38,4 +49,46 @@ class ProfileUser(SuccessMessageMixin, UpdateView):
         change_profile(request, self.get_queryset())
 
         messages.add_message(self.request, messages.INFO, _('Профиль успешно сохранен'))
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class RegShopView(SuccessMessageMixin, CreateView):
+    """Представление для регистрации магазина."""
+
+    template_name = 'account/reg_shop.j2'
+    fields = '__all__'
+    model = Shop
+
+    def get_success_url(self):
+        """Возвращаемый URL при успешном выполнении методов."""
+        return reverse_lazy('account:account_user', kwargs={'pk': self.kwargs['pk']})
+
+    def post(self, request, *args, **kwargs):
+        """Добавление магазина."""
+        message = create_shop(request=request)
+
+        messages.add_message(self.request, messages.INFO, message)
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class UpdateShopView(SuccessMessageMixin, UpdateView):
+    """Представление для редактирования магазина."""
+
+    template_name = 'account/update_shop.j2'
+    fields = '__all__'
+    context_object_name = 'shop'
+    model = Shop
+
+    def get_object(self, queryset=None):
+        """Возвращение объекта магазина."""
+        return get_object_or_404(Shop, user_id=self.kwargs.get('pk'))
+
+    def get_success_url(self):
+        """Возвращаемый URL при успешном выполнении методов."""
+        return reverse_lazy('account:update_shop', kwargs={'pk': self.kwargs['pk']})
+
+    def post(self, request, *args, **kwargs):
+        """Редактирование магазина."""
+        update_shop(request=request)
+        messages.add_message(self.request, messages.INFO, _('Магазин успешно редактирован'))
         return HttpResponseRedirect(self.get_success_url())
