9eefde5571aed5bed292ec37dfaded3044e9740f
Nikolay Osipov <nick_osipov@mail.ru>
2023-05-16 22:31:04 +0300

    Добавил отображения слайдера баннеров на главной странице.

--- market/order/urls.py
+++ market/order/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from order.views import Step1View, Step2View, Step3View, Step4View, OrderDetailView, OrderListView, Step1NAView
+from order.views import Step1View, Step2View, Step3View, Step4View, OrderDetailView, OrderListView
 
 app_name = 'order'
 
 urlpatterns = [
     path('step1/', Step1View.as_view(), name='step1'),
-    path('step1na/', Step1NAView.as_view(), name='step1na'),
     path('step2/', Step2View.as_view(), name='step2'),
     path('step3/', Step3View.as_view(), name='step3'),
     path('step4/', Step4View.as_view(), name='step4'),
--- market/order/views.py
+++ market/order/views.py
@@ -1,11 +1,8 @@
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import Group
 from django.db import transaction
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import FormView, CreateView, DetailView, ListView
 
 from cart.cart import CartServices
@@ -14,97 +11,43 @@ from order.models import Order
 from order.forms import UserForm, DeliveryForm, PaymentForm, CommentForm
 from order.services import add_items_from_cart
 from users.models import User
+from users.services import create_user
 
 
-class Step1View(LoginRequiredMixin, FormView):
+class Step1View(View):
     """
     Отображает страницу первого шага заказа
     """
     template_name = 'order/step1.j2'
     form_class = UserForm
-    success_url = reverse_lazy('order:step2')
-    login_url = reverse_lazy('order:step1na')
-
-    def get_initial(self):
-        user = self.request.user
-        full_name = f"{user.last_name} {user.first_name} {user.surname}"
-        return {'full_name': full_name, 'email': user.email, 'phone_number': user.phone_number}
-
-    def form_valid(self, form):
-        user_data = {'full_name': form.cleaned_data['full_name'],  # noqa F841
-                     'email': form.cleaned_data['email'],
-                     'phone_number': form.cleaned_data['phone_number']}
-        cart = CartServices(self.request)
-        cart.add_user_data(form)
-        return super().form_valid(form)
 
     def get(self, request, *args, **kwargs):
-        form = self.form_class(initial=self.get_initial())
-        context_data = self.get_context_data()
-        context_data['form'] = form
-        return render(request, self.template_name, context=context_data)
-
-    def dispatch(self, request, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            if 'from_step1' in request.GET:
-                return redirect('order:step2')
-            return super().dispatch(request, *args, **kwargs)
+        if request.user.is_authenticated:
+            user = User.objects.get(email=request.user.email)
+            full_name = f"{user.last_name} {user.first_name} {user.surname}"
+            form = self.form_class(initial={
+                'full_name': full_name,
+                'email': user.email,
+                'phone_number': user.phone_number
+            })
         else:
-            return redirect('order:step1na')
-
-
-class Step1NAView(FormView):
-    """
-    Отображает страницу первого шага заказа
-    """
-    template_name = 'order/step1_na.j2'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('order:step2')
+            form = self.form_class()
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
-        """После регистрации, пользователю добавляется группа с разрешениями "покупатель"."""
-        full_name = request.POST.get('name')
-        last_name, first_name, surname = full_name.split(' ')
-        email = request.POST.get('mail')
-        phone_number = request.POST.get('phone')
-        password1 = request.POST.get('password')
-        password2 = request.POST.get('passwordReply')
-
-        if User.objects.filter(email=email).exists():
-            messages.add_message(request, messages.INFO,
-                                 'Пользователь с указанным email уже существует, вы можете авторизоваться.')
-            return redirect('login')
-
-        if password1 != password2:
-            messages.add_message(request, messages.INFO, 'Пароли не совпадают!')
-            return redirect('register')
-
-        with transaction.atomic():
-            user = User.objects.create_user(
-                username=email,
-                last_name=last_name,
-                first_name=first_name,
-                surname=surname,
-                email=email,
-                phone_number=phone_number,
-                password=password1
-            )
-            group = Group.objects.get(name='buyer')
-            user.groups.add(group)
-
-            user = authenticate(email=email, password=password1)
-            login(request, user)
-
-            user_data = {'full_name': full_name,
-                         'email': email,
-                         'phone_number': phone_number}
-            user_form = UserForm(user_data)
-            if user_form.is_valid():
-                user_form.cleaned_data = user_data
-                cart = CartServices(self.request)
-                cart.add_user_data(user_form)
-
-            return redirect(self.success_url)
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user_data = {
+                'full_name': form.cleaned_data['full_name'],
+                'email': form.cleaned_data['email'],
+                'phone_number': form.cleaned_data['phone_number']
+            }
+            if not request.user.is_authenticated:
+                create_user(request, user_data)
+            cart = CartServices(request)
+            cart.add_user_data(form)
+            return redirect('order:step2')
+        return render(request, self.template_name, {'form': form})
 
 
 class Step2View(LoginRequiredMixin, FormView):
